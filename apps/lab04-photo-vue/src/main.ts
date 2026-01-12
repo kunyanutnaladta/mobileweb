6bf531d43fd981374f3723db966aecf9ede59013
Kunyanat <Kunyanat.n@kkumail.com>
2026-01-12 07:33:25 +0700

    lab4

---
+++ apps/lab04-photo-vue/src/main.ts
@@ -0,0 +1,38 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+
+import { IonicVue } from '@ionic/vue';
+
+// ✅ สำหรับ Camera / Photo (PWA)
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+/* ✅ Ionic Core CSS */
+import '@ionic/vue/css/core.css';
+
+/* ✅ Basic CSS for apps built with Ionic */
+import '@ionic/vue/css/normalize.css';
+import '@ionic/vue/css/structure.css';
+import '@ionic/vue/css/typography.css';
+
+/* ✅ Optional CSS utils */
+import '@ionic/vue/css/padding.css';
+import '@ionic/vue/css/float-elements.css';
+import '@ionic/vue/css/text-alignment.css';
+import '@ionic/vue/css/text-transformation.css';
+import '@ionic/vue/css/flex-utils.css';
+import '@ionic/vue/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+/* Load PWA elements */
+defineCustomElements(window);
+
+const app = createApp(App)
+  .use(IonicVue)
+  .use(router);
+
+router.isReady().then(() => {
+  app.mount('#app');
+});
