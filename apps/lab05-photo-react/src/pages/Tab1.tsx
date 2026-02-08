d7e60ec7b0d1058ebff75e9642e64f943cb38e23
Kunyanat <Kunyanat.n@kkumail.com>
2026-02-08 14:51:18 +0700

    lab 5

---
+++ apps/lab05-photo-react/src/pages/Tab1.tsx
@@ -0,0 +1,16 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
+const Tab1: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Tab 1</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen></IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab1;
---
+++ apps/lab05-photo-react/src/pages/Tab3.tsx
@@ -0,0 +1,16 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
+const Tab3: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Tab 3</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen></IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab3;
